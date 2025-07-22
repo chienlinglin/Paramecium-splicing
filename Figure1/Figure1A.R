f2f8c453155b9293d6a974e2ef24d11ba0ab24a1
imb-n420-cll <jagjagjag2001@gmail.com>
2025-07-22 16:00:13 +0800

    Figure 1A change

---
+++ Figure1/Figure1A.R
@@ -0,0 +1,20 @@
+# Load library
+library(tidyverse)
+
+# Import intron data 
+Intron_data <- read_tsv("Intron_data.tsv")
+
+# The middle point of intron = (end+start)/2
+Intron_data <- Intron_data |>
+  mutate(intron_midpoint=(intron_start+intron_end)/2,
+         transcript_length = abs(three_prime_end-five_prime_end),
+         intron_to_5_end = abs(intron_midpoint-five_prime_end)/transcript_length)
+
+# Figure 1A
+ggplot(intron_position_data, aes(x=intron_to_5_end)) +
+  geom_histogram(bins = 50,color="black", fill="lightblue")+
+  labs(
+    x = "Relative position to 5' end of gene",
+    y = "Number of introns"
+  )+
+  theme_bw(base_size = 10)
