3c95a8c5d28d84ce52279f9dcb3597438922ad93
Jag <jagjagjag2001@gmail.com>
2025-07-23 23:26:13 +0800

    Fig4B

--- Figure4/Figure4.R
+++ Figure4/Figure4.R
@@ -28,3 +28,47 @@ Fig4A <- ggplot(mean_PSI_sum, aes(x = GC_content_round)) +
   geom_line(aes(y = mean_PSI, color = "mean PSI"), linewidth = 1.5) +
   geom_errorbar(aes(y = mean_PSI, ymin = mean_PSI - se_PSI, ymax = mean_PSI + se_PSI), color = 'black', width = 0.015) +
   labs(x = "GC content", y = "PSI", color = "Sample")
+
+## 02. Intron retention and intron length 
+# Import intron length and PSI data 
+intron_length_PSI <- read_csv("intron_length_PSI.csv")
+
+# melt data 
+intron_length_PSI_all_long <- intron_length_PSI_all |>
+  pivot_longer(
+    cols = starts_with("avg"),
+    names_to = "samples",
+    values_to = "avg_PSI"
+  )
+
+# Divide intron length into: 15-22, 23-26, >=27
+intron_length_PSI_all_long <- intron_length_PSI_all_long |>
+  mutate(intron_length_cat = case_when(intron_width <23 ~ "15-22",
+                                       intron_width < 27 ~ "23-26",
+                                       intron_width >=27 ~ ">=27"))
+
+# Order factor level based on the length 
+intron_length_PSI_all_long$intron_length_cat <- factor(
+  intron_length_PSI_all_long$intron_length_cat,
+  levels = c("15-22", "23-26", ">=27"),
+  labels = c("15-22", "23-26", ">=27")
+)
+
+# Change >= symbol
+levels(intron_length_PSI_all_long$intron_length_cat)[3] <- "≥27"
+# Plot
+Fig4B <- ggplot(intron_length_PSI_all_long, aes(x = intron_length_cat, y = avg_PSI, fill = intron_length_cat)) +
+  geom_boxplot(outliers = FALSE, width = 0.2) +
+  #stat_n_text(angle = 45) + 
+  geom_signif(
+    comparisons = list(
+      c("15-22", "23-26"),
+      c("23-26", "≥27")
+    ),
+    map_signif_level = TRUE,
+    textsize = 6,
+    margin_top = -0.8,
+    step_increase = 0.02,
+    tip_length = 0.01
+  ) +
+  theme_bw(base_size = 12)
