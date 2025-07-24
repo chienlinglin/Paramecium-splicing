70fdbc3aaa5687bd00cd4d4f6c20a6124f8f1fb1
imb-n420-cll <jagjagjag2001@gmail.com>
2025-07-24 15:21:04 +0800

    add Figure 6 folder

---
+++ Figure6/Figure6.R
@@ -0,0 +1,10 @@
+# Load library
+library(tidyverse)
+
+## 001. Figure 6B. Intron age and PSI
+
+## 002. Figure 6C. Intron age and Intron GC content
+
+## 003. Figure 6D. Intron age and intron length
+
+## 004. Figure 6E. Maximum intron age and gene expression level
