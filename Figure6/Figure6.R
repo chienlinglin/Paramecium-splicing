77c4014272189caa1389a9bef6147fb9925ebbab
imb-n420-cll <jagjagjag2001@gmail.com>
2025-07-24 17:42:26 +0800

    update Fig6C

--- Figure6/Figure6.R
+++ Figure6/Figure6.R
@@ -50,7 +50,7 @@ Fig6B <- ggplot(Pb_intron_age_merge_PSI, aes(x = as.factor(intron_age), y = avg_
 ## 002. Figure 6C. Intron age and Intron GC content
 # Plot 
 Pb_intron_age$intron_age <- as.factor(Pb_intron_age$intron_age)
-ggplot(Pb_intron_age, aes(x = as.factor(intron_age), y = intron_gc_content, fill = as.factor(intron_age))) + 
+Fig6C <- ggplot(Pb_intron_age, aes(x = as.factor(intron_age), y = intron_gc_content, fill = as.factor(intron_age))) + 
   geom_boxplot(outliers = FALSE, width = 0.3) +
   labs(
     y = "intron content (%)",
@@ -72,7 +72,7 @@ ggplot(Pb_intron_age, aes(x = as.factor(intron_age), y = intron_gc_content, fill
     step_increase = 0.05,
     tip_length = 0.01
   )
-  
+
 ## 003. Figure 6D. Intron age and intron length
 # Perform KS test bw 1 & 2, 2 & 3 intron age group length
 # Convert intron_age to factor
