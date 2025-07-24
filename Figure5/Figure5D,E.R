2e4149e74e384b9650a8ebb241e92100911e6b24
imb-n420-cll <jagjagjag2001@gmail.com>
2025-07-24 13:57:04 +0800

    update

--- Figure5/Figure5D,E.R
+++ Figure5/Figure5D,E.R
@@ -2,4 +2,8 @@
 library(tidyverse)
 library(pheatmap)
 
-## 001. Figure 5E: beta coefficient of SF DEGs 
+## 001. Figure 5E: beta coefficient of SF DEGs
+# Import z scaled PSI of 992 DSIs
+PSI_data_all_replicates_scale_sig <- read_csv("PSI_sigRIs_all_samples_z_scaled.csv")
+
+# Import 
