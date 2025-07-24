6a12f16611193b7811ec4096b2aa942d7b943eef
imb-n420-cll <jagjagjag2001@gmail.com>
2025-07-24 16:08:42 +0800

    update

--- Figure6/Figure6.R
+++ Figure6/Figure6.R
@@ -1,7 +1,17 @@
 # Load library
 library(tidyverse)
+library(ggsignif)
 
 ## 001. Figure 6B. Intron age and PSI
+# Import intron PSI and intron age data
+Intron_age <- read_csv("PSI_intron_age.csv")
+
+# Import intron PSI data
+PSI_data <- read_csv("PSI_data.csv")
+
+# Import intron id to gene id map
+intron_id_to_gene_id_all <- read_csv("intron_id_to_gene_id_all.csv")
+
 
 ## 002. Figure 6C. Intron age and Intron GC content
 
